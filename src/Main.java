adc041363d5fc17789877daf7bd6d4a13eb56b4d
CharankumarSarlani <184059583+CharankumarSarlani@users.noreply.github.com>
2025-05-19 11:36:57 +0530

    Instantiates square class in main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 import com.models.Rectangle;
+import com.models.Square;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(1,3);
-        System.out.printf("rea of the rectangle is %d", rectangle.area());
+        System.out.printf("Area of the rectangle is %d\n", rectangle.area());
+
+        Square square = new Square(2);
+        System.out.printf("Area of the square is %d",square.area());
+
     }
 }
