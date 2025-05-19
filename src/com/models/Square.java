aafd047a12981333e074f4a507e53d8d87360022
CharankumarSarlani <184059583+CharankumarSarlani@users.noreply.github.com>
2025-05-19 11:14:28 +0530

    Adds square class

--- src/com/models/Square.java
+++ src/com/models/Square.java
@@ -1,4 +1,19 @@
 package com.models;
 
-public class Square {
+public class Square implements ClosedShape{
+    private final int side;
+
+    public Square(int side) {
+        this.side = side;
+    }
+
+    @Override
+    public int area() {
+        return side*side;
+    }
+
+    @Override
+    public int perimeter() {
+        return 2*2*side;
+    }
 }
--- test/com/models/SquareTest.java
+++ test/com/models/SquareTest.java
@@ -1,4 +1,31 @@
+package com.models;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SquareTest {
-  
+    @Test
+    void calculatesArea() {
+        Square square = new Square(12);
+        assertEquals(144,square.area());
+    }
+
+    @Test
+    void calculatesPerimeter() {
+        Square square = new Square(2);
+        assertEquals(8,square.perimeter());
+    }
+
+    @Test
+    void calculatesAreaHasSideZero() {
+        Square square = new Square(0);
+        assertEquals(0,square.area());
+    }
+
+    @Test
+    void calculatesPerimeterHasSideZero() {
+        Square square = new Square(0);
+        assertEquals(0,square.perimeter());
+    }
 }
